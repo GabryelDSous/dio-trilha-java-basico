ad8550047b9c81d86150da299e614f60c5e6c42d
Gabryel <gdeusinosousafreitas@gmail>
2025-03-30 13:28:45 -0300

    operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        double salarioMinimo = 2500.332;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+
+        // VALOR_DE_PI = 10.75;
     }
 }
